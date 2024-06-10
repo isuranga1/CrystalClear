0efc6e71f23d0d68e3f30528323e184179b8898d
JulianSilva2001 <sackjuliansilva@gmail.com>
2024-06-10 14:00:24 +0530

    Handpose and Voice detection added

---
+++ Handpose and Voice/handpose/src/Speechtotext.js
@@ -0,0 +1,12 @@
+// SpeechtoText.js
+import React from 'react';
+
+const SpeechtoText = () => {
+  return (
+    <div>
+      This is speech recognition
+    </div>
+  );
+}
+
+export default Speechtotext;
